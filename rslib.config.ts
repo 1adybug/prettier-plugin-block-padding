5010ad46e8159050c25e1b1ff2eec96649d97655
陆子虚 <lurongv@qq.com>
2025-10-15 09:53:34 +0800

    ✨feature: 添加 cjs 支持

--- rslib.config.ts
+++ rslib.config.ts
@@ -7,5 +7,10 @@ export default defineConfig({
             syntax: ["node 18"],
             dts: true,
         },
+        {
+            format: "cjs",
+            syntax: ["node 18"],
+            dts: true,
+        },
     ],
 })
